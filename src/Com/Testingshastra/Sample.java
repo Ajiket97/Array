c5e6121f47c069d140a5dfca95ba0651fb663fd4
Ajiket97 <ajitshingnare02@gmail.com>
2024-04-20 11:17:39 +0530

    todays work

---
+++ src/Com/Testingshastra/Sample.java
@@ -0,0 +1,15 @@
+package Com.Testingshastra;
+
+import java.sql.Driver;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Sample {
+
+	public static void main(String[] args) {
+		System.out.println();
+		ChromeDriver d = new ChromeDriver();
+		
+	}
+
+}
---
+++ src/Com/Testingshastra/Test.java
@@ -0,0 +1,5 @@
+package Com.Testingshastra;
+
+public @interface Test {
+
+}
